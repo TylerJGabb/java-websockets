89fc4435892ded797318c1f6f7b42ef69d558a65
tyler gabb <tgabb@mmsi.com>
2020-02-04 17:17:58 -0500

    init

---
+++ src/main/java/com/gabb/sb/App.java
@@ -0,0 +1,13 @@
+package com.gabb.sb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
