7f008d22b81737dc536c42e7400b1db22a362bec
tyler gabb <tgabb@mmsi.com>
2020-02-05 19:49:51 -0500

    able to parse frame up to payload length! need to clean up code after decoding entire frame...

--- src/main/java/com/gabb/sb/App.java
+++ src/main/java/com/gabb/sb/App.java
@@ -1,12 +1,17 @@
 package com.gabb.sb;
 
 
+import com.sun.corba.se.impl.legacy.connection.SocketFactoryConnectionImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.net.SocketFactory;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
@@ -20,6 +25,10 @@ import java.util.regex.Pattern;
 /**
  * Writing a web socket
  * https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_a_WebSocket_server_in_Java
+ * https://docs.oracle.com/javase/tutorial/networking/sockets/readingWriting.html
+ * https://tools.ietf.org/html/draft-ietf-hybi-thewebsocketprotocol-17#section-1.3
+ * https://en.wikipedia.org/wiki/Augmented_Backus%E2%80%93Naur_form
+ * https://chrome.google.com/webstore/detail/smart-websocket-client/omalebghpgejjiaoknljcfmglgbpocdp?utm_source=chrome-app-launcher-info-dialog
  */
 public class App {
 
@@ -36,24 +45,92 @@ public class App {
 		LOGGER.info("Server started on 127.0.0.1:{}, waiting for connection...", port);
 		Socket client = server.accept();
 		LOGGER.info("Client connected: {}", client);
-
-
 		InputStream in = client.getInputStream();
 		OutputStream out = client.getOutputStream();
-		Scanner scannerIn = new Scanner(in, UTF_8);
+		handshake(in, out);
 
 		/**
-		 * HANDSHAKING
-		 * actually doing the handshake is easier than understanding why its done this way
-		 * read up on websocket handshaking
+		 *       0                   1                   2                   3
+		 *       0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+		 *      +-+-+-+-+-------+-+-------------+-------------------------------+
+		 *      |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
+		 *      |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
+		 *      |N|V|V|V|       |S|             |   (if payload len==126/127)   |
+		 *      | |1|2|3|       |K|             |                               |
+		 *      +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
+		 *      |     Extended payload length continued, if payload len == 127  |
+		 *      + - - - - - - - - - - - - - - - +-------------------------------+
+		 *      |                               |Masking-key, if MASK set to 1  |
+		 *      +-------------------------------+-------------------------------+
+		 *      | Masking-key (continued)       |          Payload Data         |
+		 *      +-------------------------------- - - - - - - - - - - - - - - - +
+		 *      :                     Payload Data continued ...                :
+		 *      + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
+		 *      |                     Payload Data continued ...                |
+		 *      +---------------------------------------------------------------+
 		 */
-		
-		String data = scannerIn.useDelimiter("\\r\\n\\r\\n").next();
-		LOGGER.info("Received input: {}", data);
+		new Thread(() -> {
+			//section 5.2
+			try {
+				int size = 256;
+				byte[] buf = new byte[size];
+				int offset = 0;
+				int readCount = in.read(buf, offset, size);
+				LOGGER.info("read {} bytes", readCount);
+				byte one = buf[0];
+				byte two = buf[1];
+				byte thr = buf[2];
+				byte fou = buf[3];
+				int fin = (one & 0b10000000) >> 7;
+				int rsv1 = (one & 0b01000000) >> 6;
+				int rsv2 = (one & 0b00100000) >> 5;
+				int rsv3 = (one & 0b00010000) >> 4;
+				int opcode = (one & 0b00001111);
+				int masked = (two & 0b10000000) >> 7;
+
+
+				LOGGER.info("FIN={}", fin);
+				LOGGER.info("RSV 1,2,3 = {},{},{}", rsv1, rsv2, rsv3);
+				LOGGER.info("opcode = {}", opcode);
+				LOGGER.info("masked = {}", masked);
+
+				// now we need to get the payload length. by default this value is 7 bits
+				// 01111111 = 127  --> extended length is an additional 8 bytes
+				// 01111110 = 126  --> extended length is an additional 2 bytes 
+				int payloadLen = (two & 0b01111111);
+				if (payloadLen > 125) {
+					LOGGER.info("PAYLOAD LENGTH IS EXTENDED BY {} BYTES", payloadLen == 126 ? 2 : 8);
+					if (payloadLen == 126) {
+						//payload length is the next 23 bits (can fit inside int)
+						payloadLen = (payloadLen << 16) + (thr << 8) + fou;
+					} else {
+						//payload length is the next 71 bits 
+						LOGGER.error("EXTENDED PAYLOAD LENGTH NOT SUPPORTED YET");
+						client.close();
+					}
+				}
+
+				LOGGER.info("PAYLOAD LENGTH IS {}", payloadLen);
+				
+				// now we have to read the masking key, the next 4 bytes after the payload length
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}).start();
+
+	}
+
+	/**
+	 * HANDSHAKING
+	 * actually doing the handshake is easier than understanding why its done this way
+	 * read up on websocket handshaking
+	 */
+	private static void handshake(InputStream in, OutputStream out) throws NoSuchAlgorithmException, IOException {
+		Scanner scannerIn = new Scanner(in, UTF_8).useDelimiter("\\r\\n\\r\\n");
+		String data = scannerIn.next();
 		Matcher get = Pattern.compile("^GET").matcher(data);
 
 		if (get.find()) {
-			LOGGER.info("Received GET");
 			String webSocketAccept = getWebSocketAccept(data);
 			byte[] response = buildResponse(webSocketAccept);
 			out.write(response, 0, response.length);
@@ -70,7 +147,7 @@ public class App {
 
 	private static String getWebSocketAccept(String data) throws NoSuchAlgorithmException {
 		Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
-		if (!match.find()) return null;
+		if (!match.find()) return "REQUIRES HEADER Sec-WebSocket-Key";
 		String webSocketKey = match.group(1);
 		String linkedWebSocketKey = webSocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 		MessageDigest messageDigestSHA1 = MessageDigest.getInstance("SHA-1");
