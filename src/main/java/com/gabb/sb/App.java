780cef91be0b002748dadce94abe39792a3e9751
tyler gabb <tgabb@mmsi.com>
2020-02-09 16:42:54 -0500

    building data frame with function to send to client

--- src/main/java/com/gabb/sb/App.java
+++ src/main/java/com/gabb/sb/App.java
@@ -4,21 +4,22 @@ package com.gabb.sb;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.rmi.server.ExportException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Base64;
-import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -49,7 +50,12 @@ public class App {
 		LOGGER.info("Client connected: {}", client);
 		InputStream in = client.getInputStream();
 		OutputStream out = client.getOutputStream();
-		handshake(in, out);
+		boolean success = handshake(in, out);
+		if (success) LOGGER.info("Handshake Successful");
+		else {
+			LOGGER.info("Handshake Unsuccessful, aborting program");
+			System.exit(1);
+		}
 
 		/**
 		 *       0                   1                   2                   3
@@ -77,31 +83,25 @@ public class App {
 			//section 5.2
 			while (true) {
 				try {
-					byte finop = dataStream.readByte();
-					byte masklen = dataStream.readByte();
+					int finop = dataStream.readUnsignedByte();
+					int masklen = dataStream.readUnsignedByte();
 					boolean fin = (finop & 0x80) > 0;
 					int opcode = finop & 0xF;
 					boolean masked = (masklen & 0x80) > 0;
 					//TODO: if not masked, fail the connection
 					byte len = (byte) (masklen & 0x7F);
 					LOGGER.info("FRAME HEADER: {}", String.format("%02x %02x", finop, masklen));
-					
-					boolean shortPayload = len ==  126;
+
+					boolean shortPayload = len == 126;
 					boolean longPayload = len == 127;
 					long payloadLen = len;
 					if (shortPayload) {
 						//payload length is the next 16 bits (short)
-						payloadLen = (dataStream.readByte() << 8) | dataStream.readByte();
+						payloadLen = dataStream.readUnsignedShort();
 					} else if (longPayload) {
-						payloadLen = 0;
-						//the payload length is the next 64 bits (long)
-						for (int i = 0; i < 8; i++) {
-							//better practice to bitwise or to concatenate numbers
-							payloadLen |= (dataStream.readByte() << (7 - i));
-						}
+						payloadLen = dataStream.readLong();
 					}
 					LOGGER.info("PAYLOAD LENGTH IS {}", payloadLen);
-
 
 					byte[] mask = new byte[4];
 					if (masked) {
@@ -113,11 +113,17 @@ public class App {
 					StringBuilder payload = new StringBuilder();
 					for (int i = 0; i < payloadLen; i++) {
 						byte key = mask[i % 4];
-						byte decoded = (byte) (dataStream.readByte() ^ key);
+						byte decoded = (byte) (dataStream.readUnsignedByte() ^ key);
 						payload.append((char) decoded);
 					}
 					LOGGER.info("RECEIVED PAYLOAD:\n{}", payload);
-					out.write(TYLER);
+					StringBuilder huge = new StringBuilder();
+					Random r = new Random();
+					for(int i = 0; i < 40000; i++){
+						huge.append("A");
+					}
+					byte[] unmaskedPayload = buildUnmaskedPayload(huge.toString());
+					out.write(unmaskedPayload);
 
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -131,27 +137,53 @@ public class App {
 			}
 		}).start();
 	}
-	
-//	private byte[] buildUnmaskedFrame(String payload){
-//		List<Byte> frame = Arrays.asList((byte)0x81, (byte)0x8b);
-//		long len = payload.length();
-//	}
+
+	private static void howToReadWriteShort() throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		DataOutputStream output = new DataOutputStream(out);
+		output.writeShort(40000);
+		// several network layers later....
+		ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+		DataInputStream dataIn = new DataInputStream(in);
+		int len = dataIn.readUnsignedShort();
+		int x = 0xff;
+		System.out.println("====");
+	}
+
+	private static byte[] buildUnmaskedPayload(String payload) throws IOException {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		DataOutputStream outputStream = new DataOutputStream(out);
+		outputStream.writeByte(0x81);  // 10000001
+		int len = payload.length();
+		if (len < 125) {
+			outputStream.writeByte(len & 0xFF);
+		} else if ((len & 0xffff0000) == 0) { //len fits in a short
+			// 01111110 = 126 = 0x7E
+			outputStream.writeByte(0x7e);
+			outputStream.writeShort(len);
+		} else {
+			// 01111111 = 127 = 0x7F
+			outputStream.writeByte(0x7f);
+			outputStream.writeLong(len);
+		}
+		outputStream.writeBytes(payload);
+		return out.toByteArray();
+	}
 
 	/**
 	 * HANDSHAKING
 	 * actually doing the handshake is easier than understanding why its done this way
 	 * read up on websocket handshaking
 	 */
-	private static void handshake(InputStream in, OutputStream out) throws NoSuchAlgorithmException, IOException {
+	private static boolean handshake(InputStream in, OutputStream out) throws NoSuchAlgorithmException, IOException {
 		Scanner scannerIn = new Scanner(in, UTF_8).useDelimiter("\\r\\n\\r\\n");
 		String data = scannerIn.next();
 		Matcher get = Pattern.compile("^GET").matcher(data);
-
-		if (get.find()) {
-			String webSocketAccept = getWebSocketAccept(data);
-			byte[] response = buildResponse(webSocketAccept);
-			out.write(response, 0, response.length);
-		}
+		if (!get.find()) return false;
+		String webSocketAccept = getWebSocketAccept(data);
+		byte[] response = buildResponse(webSocketAccept);
+		out.write(response, 0, response.length);
+		return webSocketAccept != null;
 	}
 
 	private static byte[] buildResponse(String webSocketAccept) {
@@ -164,7 +196,7 @@ public class App {
 
 	private static String getWebSocketAccept(String data) throws NoSuchAlgorithmException {
 		Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
-		if (!match.find()) return "REQUIRES HEADER Sec-WebSocket-Key";
+		if (!match.find()) return null;
 		String webSocketKey = match.group(1);
 		String linkedWebSocketKey = webSocketKey + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
 		MessageDigest messageDigestSHA1 = MessageDigest.getInstance("SHA-1");
